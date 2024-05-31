9907c568507fe3c7e906b14303cb4fcfc04f0854
Asbestosstar <ricsch97@gmail.com>
2024-05-31 12:56:58 -0700

    Update EarlyWindow.java

--- src/main/java/org/mangorage/mangobot/modules/logs/EarlyWindow.java
+++ src/main/java/org/mangorage/mangobot/modules/logs/EarlyWindow.java
@@ -16,11 +16,7 @@ public class EarlyWindow  implements LogAnalyserModule{
 			String last = lines[lines.length - 1];
 			if (last.contains("Loading ImmediateWindowProvider fmlearlywindow")) {
 				message.reply("Your FML Early Window is failing. "
-						+ "This is in many cases caused by broken drivers. If you have a graphics card from NVIDIA, make sure you set MinecraftLauncher and all instances of Minecraft's javaw.exe files. If you do not have NVIDIA and have only AMD, you can update your drivers. [Guide](https://forums.minecraftforge.net/topic/125488-rules-and-frequently-asked-questions-faq/#:~:text=How%20do%20I%20update%20my%20drivers%3F)"+nl
-						+ "If you are on an M1 Mac you should also make sure you are using an ARM version of Java not an Intel x64 one."+nl
-						+ "If it still does not work after all these steps you can try to turn off the earlyWindowProver, though this can cause some issues with some mods."+nl		
-						+ "To do this go to (.)minecraft/config/fml.toml"+nl
-						+ "Edit earlyWindowProvider to be earlyWindowProvider=\"none\"")
+						+ "This is in many cases caused by broken drivers. If you have a graphics card from NVIDIA, make sure you set MinecraftLauncher and all instances of Minecraft's javaw.exe files to use your dedicated GPU. If you do not have NVIDIA and have only AMD, you can update your drivers. [Guide](https://forums.minecraftforge.net/topic/125488-rules-and-frequently-asked-questions-faq/#:~:text=How%20do%20I%20update%20my%20drivers%3F)"+nl
 						.setSuppressEmbeds(true).mentionRepliedUser(true).queue();
 			}
 		}
