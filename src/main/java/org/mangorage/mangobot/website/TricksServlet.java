0832d2dfc88fa702ce626e48a9189c891fa859bb
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-02-05 04:42:52 -0800

    Fixed Styles

--- src/main/java/org/mangorage/mangobot/website/TricksServlet.java
+++ src/main/java/org/mangorage/mangobot/website/TricksServlet.java
@@ -46,6 +46,20 @@ public class TricksServlet extends AbstractServlet {
         var str3 = "<html><body><h2>%s</h2></body></html>";
         var str4 = "<html><body><h4>%s</h4></body></html>";
 
+
+        response.getWriter().write(
+                """
+                <!DOCTYPE html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="styles.css">
+                </head>
+                </html>
+                """
+        );
+
         if (guildId != null && trickId != null) {
             try {
                 Trick trick = command.getTrick(trickId, Long.parseLong(guildId));
