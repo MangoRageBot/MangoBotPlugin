8e8b868680333932f4860c365470c22175981052
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-03-21 23:08:54 -0700

    Fixed Issue

--- src/main/java/org/mangorage/mangobot/modules/logs/LogAnalyserModule.java
+++ src/main/java/org/mangorage/mangobot/modules/logs/LogAnalyserModule.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface LogAnalyserModule {
 
 	static String[] split(String str) {
-		return str.lines().toArray(String[]::new);
+		return str.replace("\r\n", "\n").replace("\r", "\n").split("\n");
 	}
 
 	LogAnalyser MAIN = LogAnalyser.of(
