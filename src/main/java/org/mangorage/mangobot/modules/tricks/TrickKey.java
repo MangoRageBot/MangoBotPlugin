b63bb798503d84ac90a8c1bf7327c3eecda496a6
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2025-04-21 16:25:02 -0700

    Initial Rewrite

--- src/main/java/org/mangorage/mangobot/modules/tricks/TrickKey.java
+++
@@ -1,3 +0,0 @@
-package org.mangorage.mangobot.modules.tricks;
-
-public record TrickKey(String trickId, long guildId) {}
--- src/main/java/org/mangorage/mangobot/modules/tricks/TrickType.java
+++
@@ -1,7 +0,0 @@
-package org.mangorage.mangobot.modules.tricks;
-
-public enum TrickType {
-    NORMAL,
-    ALIAS,
-    SCRIPT
-}
